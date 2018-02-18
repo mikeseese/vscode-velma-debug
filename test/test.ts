f3ae631b3e79cbd05748e4fec1e70e84fb619591
Mike Seese <seesemichaelj@gmail.com>
2018-02-18 13:38:38 -0500

    initial test code

---
+++ test/test.ts
@@ -0,0 +1,15 @@
+
+//
+
+describe("SdbTest", () => {
+    before(async () => {
+        // initialize testrpc/ganache-core
+        // compile contract
+        // upload contract
+        // link debug symbols
+    });
+
+    it("#sdbTest", async () => {
+        //
+    });
+})
